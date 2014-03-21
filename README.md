7b1be4d7d2473065db0d99c1a495557e51c96d63
hupantingxue <hupantingxue@126.com>
2014-03-21 21:14:30 +0800

    Modify runserver script;

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 
 
 <h3>How to run</h3>
+python manage.py runserver listen_host:port
 
--- WebOrdering/runserver.sh
+++ WebOrdering/runserver.sh
@@ -1,3 +1,1 @@
-#!/etc/bin/bash
-python manage.py runserver &
-google-chrome http://127.0.0.1:8000 &
+python manage.py runserver 0.0.0.0:8900
